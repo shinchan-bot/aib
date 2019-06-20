cd2167a674b376d72e9d63db32526e97eaa75f37
thewebsitelevel <43106203+thewebsitelevel@users.noreply.github.com>
2019-06-20 16:33:38 +0530

    Updates

---
+++ precache-manifest.c3a6126db6acc759980c65750b797692.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1b45b328c02058cb9684ab020231ba",
+    "url": "/aib/index.html"
+  },
+  {
+    "revision": "bb77d5f9fe75329a2103",
+    "url": "/aib/static/css/2.80c66fb8.chunk.css"
+  },
+  {
+    "revision": "8369c9204da70d2c0b34",
+    "url": "/aib/static/css/main.5b8955e6.chunk.css"
+  },
+  {
+    "revision": "bb77d5f9fe75329a2103",
+    "url": "/aib/static/js/2.6b7befe8.chunk.js"
+  },
+  {
+    "revision": "8369c9204da70d2c0b34",
+    "url": "/aib/static/js/main.748bc7e9.chunk.js"
+  },
+  {
+    "revision": "4a7e075f216939ea7a46",
+    "url": "/aib/static/js/runtime~main.7985bf50.js"
+  },
+  {
+    "revision": "ae7b35ce5c3655b8fd05c28bcfb0301d",
+    "url": "/aib/static/media/1.ae7b35ce.jpg"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/aib/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "b090e2b33c430fb96c49e49a05b43a5c",
+    "url": "/aib/static/media/about-us.b090e2b3.png"
+  },
+  {
+    "revision": "a8cac2a53f88d93cf8f7424023843e61",
+    "url": "/aib/static/media/brick-logo.a8cac2a5.jpg"
+  },
+  {
+    "revision": "473609fb3ad9766fcc774e634a293559",
+    "url": "/aib/static/media/location.473609fb.png"
+  },
+  {
+    "revision": "baea5a801d64f8245d33f38b47f97121",
+    "url": "/aib/static/media/mtb-clay-bricks.baea5a80.jpg"
+  },
+  {
+    "revision": "6b5fd17848f8e1a0771202a14a772f87",
+    "url": "/aib/static/media/president.6b5fd178.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/aib/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/aib/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/aib/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/aib/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "d0eb46f92b0f61ebf54a496e331d8f3d",
+    "url": "/aib/static/media/transparent.d0eb46f9.png"
+  }
+]);
