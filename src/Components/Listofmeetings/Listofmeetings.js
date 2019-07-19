e00b3a37c336adcf2a7ec42c98920ca210a59223
thewebsitelevel <43106203+thewebsitelevel@users.noreply.github.com>
2019-07-19 13:49:42 +0530

    A

--- src/Components/Listofmeetings/Listofmeetings.js
+++ src/Components/Listofmeetings/Listofmeetings.js
@@ -47,7 +47,7 @@ class Listofmeetings extends Component {
         }
         const meeting_array = meetingPostsstore.map((user,num) =>{
             return(
-                <div className="bl meetingmobile bb b--gray br4 mb2 shadow-2 pa2">
+                <div className="bl meetingmobile bb b--gray br3 mb2 shadow-2 pa2">
                     <p className='meeting_date' data-key={user} key={user} onClick={() =>meeting_handler(user)} className="red link pointer dim ">DATE: <i>{user.date}</i></p>
                     <p  className="meet">{user.description}</p>
                 </div> 
@@ -57,7 +57,7 @@ class Listofmeetings extends Component {
         const data = (
                 this.state.meetingPosts 
                 ?
-                    <div className="meeting_details bt bb bw2 b--black br4 mb2 mt4 pa2">
+                    <div className="meeting_details ba bw2 b--black br3 mb2 mt4 pa2">
                         <h3 className="meeting_title fw8">{this.state.num.title}</h3>
                         <p className='meeting_description  fw5 red'><i>{this.state.num.description}</i></p>
                         <p className="meeting_detail fw7 mt4 tj "><i>{this.state.num.details}</i></p>
@@ -76,8 +76,8 @@ class Listofmeetings extends Component {
                     MEETINGS
                 </h1>
                 <div className="meeting_box_combine  b--black" >
-                    <div className="meeting_list_box bt bb bw1 b--black br4 shadow-5">
-                        <h3 className="meeting_list_header f3 fw8 ba b--gray bw1 shadow-2 br4 pa2 tc bg-white ">Meetings in the past.</h3>
+                    <div className="meeting_list_box bt bb bw1 b--black br3 shadow-5">
+                        <h3 className="meeting_list_header f3 fw8 ba b--gray bw1 shadow-2 br3 pa2 tc bg-white ">Meetings in the past.</h3>
                         {meeting_array}
                     </div>
                     {data}  
--- src/Components/Newscontent/Newscontent.js
+++ src/Components/Newscontent/Newscontent.js
@@ -38,11 +38,11 @@ class Newscontent extends Component {
         }
         const news_array = newsPostsstore.map((user,num) =>{
             return(
-                <div className=" bl bb b--gray br4 mb2 shadow-2 pa2">
+                <div className=" bl bb b--gray br3 mb2 shadow-2 pa2">
                     
                     <p data-key={num} key={num} onClick={() =>news_handler(num)} className="red   ">Date: <i>{newsPostsstore[num].date}</i></p>
                     <p  className="meet f4 fw6">News: {newsPostsstore[num].heading}</p>
-                    <p  className="meet f5 ">Description: {newsPostsstore[num].description}</p>
+                    <p  className="meet red f5 ">Description: {newsPostsstore[num].description}</p>
                     <p  className="meet f6 fw6">{newsPostsstore[num].details}</p>
 
                 </div> 
@@ -55,7 +55,7 @@ class Newscontent extends Component {
                 <h1 className="tc  news_header">
                     NEWS
                 </h1>
-                    <div className="meeting_list_box news_list_box w-100 bt bb bw1 b--black br4 shadow-5">
+                    <div className="meeting_list_box news_list_box w-100 bt bb bw1 b--black br3 shadow-5">
                         {news_array}
                     </div>
             </div>
