4ce861d604a4809d6ba7386c049ca99ba056de7b
thewebsitelevel <43106203+thewebsitelevel@users.noreply.github.com>
2019-08-06 17:55:43 +0530

    A

--- src/Components/Googlemaps/Googlemaps.js
+++ src/Components/Googlemaps/Googlemaps.js
@@ -1,40 +1,44 @@
 import React, {Component} from 'react';
 import 'tachyons';
-import GoogleMapReact from 'google-map-react';
+// import GoogleMapReact from 'google-map-react';
 require("dotenv").config();
 
-const style = {
-    width: '100%',
-    height: '100%'
-  }
+// const style = {
+//     width: '100%',
+//     height: '100%'
+//   }
 
 
 class Googlemaps extends Component {
-    static defaultProps = {
-        center: {
-          lat: 28.590302,
-          lng: 77.225778
-        },
-        zoom: 18
-      };
+//     static defaultProps = {
+//         center: {
+//           lat: 28.590302,
+//           lng: 77.225778
+//         },
+//         zoom: 18
+//       };
     
     render(){
-      const API_KEY="AIzaSyBPZVASh86UGWQhve9wKKSTLzQXepHozic"
 
         return(
-            <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-                defaultCenter={this.props.center}
-                defaultZoom={this.props.zoom}
+            // <div style={{ height: '100vh', width: '100%' }}>
+            //     <GoogleMapReact
+            //     bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+            //     defaultCenter={this.props.center}
+            //     defaultZoom={this.props.zoom}
+            //     >
+            //     <GoogleMapReact
+            //         lat={28.590302}
+            //         lng={77.225778}
+            //         text="ALL INDIA BRICKS AND TILE MANUFACTURING FEDERATION"
+            //     />
+            //     </GoogleMapReact>
+            // </div>
+            <iframe
+                className='home_map'
+                src={process.env.REACT_APP_API_KEY}
                 >
-                <GoogleMapReact
-                    lat={28.590302}
-                    lng={77.225778}
-                    text="ALL INDIA BRICKS AND TILE MANUFACTURING FEDERATION"
-                />
-                </GoogleMapReact>
-            </div>
+            </iframe>
 
         );
     }
