941f00e0be85dd81dac9c69bbb67e60c6488fd75
thewebsitelevel <43106203+thewebsitelevel@users.noreply.github.com>
2019-10-15 15:42:11 +0530

    ANIMATION AT EXECUTIOVE PAGE

--- src/Components/Listofexecutives/Listofexecutives.js
+++ src/Components/Listofexecutives/Listofexecutives.js
@@ -1,6 +1,7 @@
 import React,{ Component } from 'react';
 import 'tachyons';
 import { bounceInLeft } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
 
 import '../../util/css/app.css';
 
@@ -14,23 +15,32 @@ class Listofexecutives extends Component {
 
     render(){
 
+        const styles = {
+            bounceInLeft: {
+              animation: 'x 2s',
+              animationName: Radium.keyframes(bounceInLeft, 'bounceInLeft')
+            }
+          }
+
         const executive_array = exec.executives.map((executive, i) =>{
             return (
-                <div >
-                    <p className='pl4 executive_state b-- shadow-4 br3 bg-gray white'>{executive.FIELD7}</p>
-                    <ul className='executive_list ba b--white br3 shadow-3 pt2 pb2 w-90 ml4'>
-                        <li className="executive_text fw7"><span>Name : </span>{executive.FIELD1}</li>
-                        <li className=" executive_text fw4 green" ><span className='executive_text fw7'></span>{executive.FIELD2}</li>
-                        <li className='executive_text'><span className="executive_text fw7">Contact : </span>{executive.FIELD3}</li>
-                        <ul className='executive_list'>
+                <StyleRoot>
+                    <div style={styles.bounceInLeft}>
+                        <p className='pl4 executive_state b-- shadow-4 br3 bg-gray white'>{executive.FIELD7}</p>
+                        <ul className='executive_list ba b--white br3 shadow-3 pt2 pb2 w-90 ml4'>
+                            <li className="executive_text fw7"><span>Name : </span>{executive.FIELD1}</li>
+                            <li className=" executive_text fw4 green" ><span className='executive_text fw7'></span>{executive.FIELD2}</li>
+                            <li className='executive_text'><span className="executive_text fw7">Contact : </span>{executive.FIELD3}</li>
                             <ul className='executive_list'>
-                                <li className='executive_text'>{executive.FIELD4}</li>
-                                <li className="fw6 executive_text">{executive.FIELD5}</li>
-                                <li className='fw6 executive_text'>{executive.FIELD6}</li>
+                                <ul className='executive_list'>
+                                    <li className='executive_text'>{executive.FIELD4}</li>
+                                    <li className="fw6 executive_text">{executive.FIELD5}</li>
+                                    <li className='fw6 executive_text'>{executive.FIELD6}</li>
+                                </ul>
                             </ul>
-                         </ul>
-                    </ul>
-                </div>
+                        </ul>
+                    </div>
+                </StyleRoot>
             )
         
         })
