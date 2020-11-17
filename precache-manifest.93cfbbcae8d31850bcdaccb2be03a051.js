e408a139a9aa15427b49b7c602cdf40572dacd4e
thewebsitelevel <itskumarravinder@gmail.com>
2020-11-17 19:56:50 +0530

    Updates

---
+++ precache-manifest.93cfbbcae8d31850bcdaccb2be03a051.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ceb0e23183c1ab4f035",
+    "url": "/aib/static/css/main.9be65d5b.chunk.css"
+  },
+  {
+    "revision": "2ceb0e23183c1ab4f035",
+    "url": "/aib/static/js/main.30babdbe.chunk.js"
+  },
+  {
+    "revision": "4a7e075f216939ea7a46",
+    "url": "/aib/static/js/runtime~main.7985bf50.js"
+  },
+  {
+    "revision": "5c88ed63d0ed7bd039bf",
+    "url": "/aib/static/css/2.ad3d9e21.chunk.css"
+  },
+  {
+    "revision": "5c88ed63d0ed7bd039bf",
+    "url": "/aib/static/js/2.1047a919.chunk.js"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/aib/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "6b5fd17848f8e1a0771202a14a772f87",
+    "url": "/aib/static/media/president.6b5fd178.jpg"
+  },
+  {
+    "revision": "473609fb3ad9766fcc774e634a293559",
+    "url": "/aib/static/media/location.473609fb.png"
+  },
+  {
+    "revision": "b77682214b94c79cb235a753e45b0e0f",
+    "url": "/aib/static/media/twitter.b7768221.svg"
+  },
+  {
+    "revision": "5402c34f775e7adc297bcec405a81ddc",
+    "url": "/aib/static/media/facebook.5402c34f.svg"
+  },
+  {
+    "revision": "c682a67892b39d6986e8fee1a2c17360",
+    "url": "/aib/static/media/face2.c682a678.jpg"
+  },
+  {
+    "revision": "ef09af6df6cc105da2967ec77330022f",
+    "url": "/aib/static/media/face3.ef09af6d.png"
+  },
+  {
+    "revision": "d48263aec4affa506cf0498a1d799171",
+    "url": "/aib/static/media/brick.d48263ae.png"
+  },
+  {
+    "revision": "bb46844078265fc81f03c30cd76f6629",
+    "url": "/aib/static/media/hqdefault.bb468440.png"
+  },
+  {
+    "revision": "951791946c62ac0e48215703018c654b",
+    "url": "/aib/static/media/sketch-of-a-fixed-chimney-bulls-trench-kiln-fcbtk.95179194.png"
+  },
+  {
+    "revision": "ae7b35ce5c3655b8fd05c28bcfb0301d",
+    "url": "/aib/static/media/1.ae7b35ce.jpg"
+  },
+  {
+    "revision": "baea5a801d64f8245d33f38b47f97121",
+    "url": "/aib/static/media/mtb-clay-bricks.baea5a80.jpg"
+  },
+  {
+    "revision": "d0eb46f92b0f61ebf54a496e331d8f3d",
+    "url": "/aib/static/media/transparent.d0eb46f9.png"
+  },
+  {
+    "revision": "86fc2e2381b245941908e573ff43e426",
+    "url": "/aib/static/media/about-us.86fc2e23.png"
+  },
+  {
+    "revision": "a8cac2a53f88d93cf8f7424023843e61",
+    "url": "/aib/static/media/brick-logo.a8cac2a5.jpg"
+  },
+  {
+    "revision": "4454a2b6a8444a0b77e15c2d01a33315",
+    "url": "/aib/static/media/brick-making.4454a2b6.png"
+  },
+  {
+    "revision": "7a3c8fc2bf2deace9f3819fc6f985989",
+    "url": "/aib/static/media/bg3.7a3c8fc2.png"
+  },
+  {
+    "revision": "4414556778df8e1b1b5b02eb93e08441",
+    "url": "/aib/static/media/gallery_demo_image.44145567.jpg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/aib/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/aib/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/aib/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/aib/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "a9dfa0c81f5332a7daf136929ae63578",
+    "url": "/aib/static/media/bg20.a9dfa0c8.jpg"
+  },
+  {
+    "revision": "bca1b60a8c85bb1db4c87514587aa09c",
+    "url": "/aib/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/aib/precache-manifest.e99913748ebdc306b9a1284bae800b4f.js"
+  "/aib/precache-manifest.93cfbbcae8d31850bcdaccb2be03a051.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aib/index.html"), {
+workbox.routing.registerNavigationRoute("/aib/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
